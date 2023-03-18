1870f2b9d4a237f575f63e08503c48a62a5d7a5c
yutaro <1212yutaro@gmail.com>
2023-03-18 23:09:10 +0900

    test15

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -86,7 +86,31 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/menu.js') }}"></script>
+    <!-- <script src="{{ asset('js/menu.js') }}"></script> -->
+    <script>
+      // MENUボタンtoggle
+$(function(){
+  $('.js-nav-open-btn').on('click', function(){
+    $('.header').addClass('none');
+    $('.js-menu-container').addClass('open');
+  });
+
+  $(document).on('click', '.js-nuv-close-btn', function(){
+    $('.header').removeClass('none');
+    $('.js-menu-container').removeClass('open');
+  });
+
+});
+
+// フッターをページ最下部に固定
+$(function(){
+  var $ftr = $('.footer');
+  if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
+    $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) + 'px;' });
+  }
+});
+
+    </script>
 </body>
 </html>
 
